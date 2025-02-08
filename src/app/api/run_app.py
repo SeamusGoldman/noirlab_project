427a27f9fd2ecbd9d20505ffbbbfe5efdaad9ccc
Seamus Goldman <seamusgoldman@gmail.com>
2025-02-08 12:41:21 -0700

    had to changed pyproject.toml to include the project app

--- src/app/api/run_app.py
+++ src/app/api/run_app.py
@@ -2,15 +2,11 @@ from fastapi import FastAPI
 
 from app.api import celestial_routes
 
-# # Ensure `src/` is in Python's import path
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
-
 app = FastAPI()
-
 app.include_router(celestial_routes.router)
 
-# if __name__ == "__main__":
-#     import uvicorn
 
-#     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+def main():
+    import uvicorn
+
+    uvicorn.run("app.api.run_app:app", host="0.0.0.0", port=8000, reload=True)
